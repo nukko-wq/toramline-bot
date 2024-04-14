bd3b74e0c63b0c965ad9ac0976cd5d15ff77127f
nukko <shouta.tsuboi@gmail.com>
2024-04-14 12:14:29 +0900

    ボスデータの修正

--- api/service/toramBossData.js
+++ api/service/toramBossData.js
@@ -5,8 +5,8 @@ const bossData = [
     "attribute": "水",
     "critical_resistance": 30,
     "flee": 573,
-    "kinshi": "\n怯み ◯\n転倒 ◯\n気絶 ◯",
-    "other1": "HP50%で無敵(ストーム中)\n妨害全て☓カウンターはなし",
+    "kinshi": "\n怯み: ◯\n転倒: ◯\n気絶: ◯",
+    "other1": "※HP50%で無敵 (ストーム中)妨害全て☓カウンターはなし",
     "other2": ""
   },
   {
@@ -15,8 +15,8 @@ const bossData = [
     "attribute": "地",
     "critical_resistance": 35,
     "flee": 386,
-    "kinshi": "\n怯み ◯\n転倒 ◯\n部位破壊(頭) 気絶 ◯",
-    "other1": "モーションによって妨害無敵有",
+    "kinshi": "\n怯み: ◯\n転倒: ◯\n気絶: 部位破壊(頭)◯",
+    "other1": "※モーションによって妨害無敵有",
     "other2": ""
   },
   {
@@ -25,9 +25,9 @@ const bossData = [
     "attribute": "水",
     "critical_resistance": 30,
     "flee": 394,
-    "kinshi": "気絶 △ (硬化) ダメージ1\n転倒 △ (硬化) ダメージ1\n怯み ◯",
-    "other1": "HP50%で無敵、モブ2体出現、竜巻左右に6つ出現",
-    "other2": "左モブを倒すと左3つ竜巻消滅\n右モブを倒すと右3つ竜巻消滅"
+    "kinshi": "気絶: △ (硬化) ダメージ1\n転倒: △ (硬化) ダメージ1\n怯み: ◯",
+    "other1": "※HP50%で無敵、モブ2体出現、竜巻左右に6つ出現",
+    "other2": "※左モブを倒すと左3つ竜巻消滅\n※右モブを倒すと右3つ竜巻消滅"
   },
   {
     "name": "バテュード",
@@ -35,7 +35,7 @@ const bossData = [
     "attribute": "闇",
     "critical_resistance": 35,
     "flee": 390,
-    "kinshi": "1段階目\n気絶: ◯\n転倒: ☓\n怯み: ◯\n2段階目\n気絶: ☓\n転倒: ◯\n怯み: ◯",
+    "kinshi": "1段階目\n気絶: ◯\n転倒: ☓\n怯み: ◯\n\n2段階目\n気絶: ☓\n転倒: ◯\n怯み: ◯",
     "other1": "※ダメージ上限100万",
     "other2": "※前半: 気絶、怯みで上限30秒間開放(ゲージでリセット)\n※後半: 転倒、怯みで上限30秒間開放"
   },
@@ -55,8 +55,8 @@ const bossData = [
     "attribute": "水",
     "critical_resistance": "\n水属性時 30\n闇属性時 40",
     "flee": 527,
-    "kinshi": "怯み ◯\n水属性時気絶 ☓\n闇属性時 転倒 ☓",
-    "other1": "HP50%または追加のボールが2体出現でHPにロック。ボールを全て倒すとHPロック解除。",
+    "kinshi": "怯み: ◯\n気絶: 水属性時☓\n転倒: 闇属性時☓",
+    "other1": "※HP50%または追加のボールが2体出現でHPにロック。ボールを全て倒すとHPロック解除。",
     "other2": ""
   },
   {
@@ -65,7 +65,7 @@ const bossData = [
     "attribute": "地",
     "critical_resistance": 20,
     "flee": 416,
-    "kinshi": "前半(肩が青色)\n転倒: ◯\n怯み: ◯\n気絶: ◯\n肩が黄色または赤色\n転倒: ☓\n怯み: ☓\n気絶: ☓",
+    "kinshi": "前半(肩が青色)\n転倒: ◯\n怯み: ◯\n気絶: ◯\n\n肩が黄色または赤色\n転倒: ☓\n怯み: ☓\n気絶: ☓",
     "other1": "※残りHPによってモードが変化\nHP100%～50%: 青モード\nHP50%～25%: 黃モード\nHP25%～0%: 赤モード",
     "other2": "※黃モードへ移行後20秒以内に残HPを25%以下に出来なかった場合、最大HPの半分の数値分HPを回復後、赤モードへ移行"
   },
@@ -76,8 +76,8 @@ const bossData = [
     "critical_resistance": 20,
     "flee": 350,
     "kinshi": "怯み: ◯ カウンター発火5m以内\n転倒: ◯ カウンター発火5m以内\n気絶: ◯ カウンター発火5m以内",
-    "other1": "HPを半分削ると強制発火攻撃(一回)",
-    "other2": "発火攻撃: イグネウスから白い波紋\n5m以内にいる全員対象"
+    "other1": "※HPを半分削ると強制発火攻撃(一回)",
+    "other2": "※発火攻撃: イグネウスから白い波紋\n5m以内にいる全員対象"
   },
   {
     "name": "ミミューガ",
@@ -85,7 +85,7 @@ const bossData = [
     "attribute": "地",
     "critical_resistance": 35,
     "flee": 355,
-    "kinshi": "怯み ◯\n転倒: ◯\n気絶: ☓\nHP50%以下\n怯み: ◯\n転倒: ☓\n気絶: ☓",
+    "kinshi": "怯み ◯\n転倒: ◯\n気絶: ☓\n\nHP50%以下\n怯み: ◯\n転倒: ☓\n気絶: ☓",
     "other1": "※部位破壊1箇所ごとにDEF,MDEFが低下",
     "other2": ""
   },
@@ -95,7 +95,7 @@ const bossData = [
     "attribute": "地",
     "critical_resistance": "",
     "flee": "",
-    "kinshi": "\n怯み: ◯\n転倒: ◯\n気絶: ☓",
+    "kinshi": "怯み: ◯\n転倒: ◯\n気絶: ☓",
     "other1": "※部位破壊でノクバ→上限(10万)\n100万ほどダメージを与えると無敵→無敵後上限解除",
     "other2": "※上限中に妨害を入れると魔力爆発"
   }
