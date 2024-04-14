310fa5adaccfc7b802b169b467643c749ea3d08d
nukko <shouta.tsuboi@gmail.com>
2024-04-14 10:27:11 +0900

    ボスデータの更新

--- api/service/toramBossData.js
+++ api/service/toramBossData.js
@@ -5,8 +5,8 @@ const bossData = [
     "attribute": "水",
     "critical_resistance": 30,
     "flee": 573,
-    "kinshi": "",
-    "other1": "",
+    "kinshi": "\n怯み ◯\n転倒 ◯\n気絶 ◯",
+    "other1": "HP50%で無敵(ストーム中)\n妨害全て☓カウンターはなし",
     "other2": ""
   },
   {
@@ -15,8 +15,8 @@ const bossData = [
     "attribute": "地",
     "critical_resistance": 35,
     "flee": 386,
-    "kinshi": "",
-    "other1": "",
+    "kinshi": "\n怯み ◯\n転倒 ◯\n部位破壊(頭) 気絶 ◯",
+    "other1": "モーションによって妨害無敵有",
     "other2": ""
   },
   {
@@ -25,9 +25,9 @@ const bossData = [
     "attribute": "水",
     "critical_resistance": 30,
     "flee": 394,
-    "kinshi": "",
-    "other1": "",
-    "other2": ""
+    "kinshi": "気絶 △ (硬化) ダメージ1\n転倒△ (硬化) ダメージ1\n怯み ◯",
+    "other1": "HP50%で無敵、モブ2体出現、竜巻左右に6つ出現",
+    "other2": "左モブを倒すと左3つ竜巻消滅\n右モブを倒すと右3つ竜巻消滅"
   },
   {
     "name": "バテュード",
@@ -35,7 +35,7 @@ const bossData = [
     "attribute": "闇",
     "critical_resistance": 35,
     "flee": 390,
-    "kinshi": "",
+    "kinshi": "1段階目\n気絶 ◯\n転倒 ☓\n怯み ◯\n★気絶、怯みで上限30秒間開放\nゲージでリセット\n2段階目\n気絶 ☓\n転倒 ◯\n怯み ◯\n★転倒、怯みで上限30秒間解放",
     "other1": "",
     "other2": ""
   },
@@ -45,7 +45,7 @@ const bossData = [
     "attribute": "風",
     "critical_resistance": 20,
     "flee": 400,
-    "kinshi": "",
+    "kinshi": "怯み ◯\n転倒 ◯\n気絶 カウンター",
     "other1": "",
     "other2": ""
   },
@@ -55,8 +55,8 @@ const bossData = [
     "attribute": "水",
     "critical_resistance": "\n水属性時 30\n闇属性時 40",
     "flee": 527,
-    "kinshi": "",
-    "other1": "",
+    "kinshi": "怯み ◯\n水属性時気絶 ☓\n闇属性時 転倒 ☓",
+    "other1": "HP50%または追加のボールが2体出現でHPにロック。ボールを全て倒すとHPロック解除",
     "other2": ""
   },
   {
