0cb369a6a959939f9c7c7a51b568b69b7eecfc54
nukko <shouta.tsuboi@gmail.com>
2024-04-14 11:52:05 +0900

    改行の追加

--- api/service/formatResponseMessage.js
+++ api/service/formatResponseMessage.js
@@ -4,7 +4,7 @@ export const formatResponseMessage = (bossData) => {
   bossData.forEach((boss) => {
     const other1 = boss.other1 ? `\n\n${boss.other1}` : ""
     const other2 = boss.other2 ? `\n${boss.other2}` : ""
-    response += `${boss.displayName}\n------------------------\n属性: ${boss.attribute}\nクリティカル耐性: ${boss.critical_resistance}\nFLEE: ${boss.flee} (Normalの値)\n禁止: ${boss.kinshi}${other1}${other2}`
+    response += `${boss.displayName}\n------------------------\n属性: ${boss.attribute}\nクリティカル耐性: ${boss.critical_resistance}\nFLEE: ${boss.flee} (Normalの値)\n禁止:\n${boss.kinshi}${other1}${other2}`
   })
   return response
 }
